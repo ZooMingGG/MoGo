4735164f83752b09f92e310b716f2a41bcf26029
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-01-15 19:24:31 +0200

    did a js part of acordeon

--- js/index.js
+++ js/index.js
@@ -5,3 +5,21 @@ function showNav() {
     let nav = document.getElementById('nav');
     nav.classList.toggle('nav-active');
 }
+
+function showAcordeonContent() {
+    let firstAcordeon = document.getElementById('acordeon-1'),
+    secondAcordeon = document.getElementById('acordeon-2');
+    firstAcordeon.classList.toggle('active');
+    secondAcordeon.classList.toggle('active');
+}
+
+function showSecondAcordeonContent() {
+    let secondAcordeon = document.getElementById('acordeon-2');
+    secondAcordeon.classList.toggle('active');
+}
+
+function showThirdAcordeonContent() {
+    let thirdAcordeon = document.getElementById('acordeon-3');
+    thirdAcordeon.classList.toggle('active');
+}
+
